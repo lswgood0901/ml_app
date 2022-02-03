db3b9754131325bce497196f5384b40ca9fe8ab7
lswgood0901 <lswgood0901@gmail.com>
2022-02-03 23:13:12 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 blog</div>
+        <div>seungwon react tutorial</div>
       </div>
     </div>
   );
